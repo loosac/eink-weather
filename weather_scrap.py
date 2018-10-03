66eb3ad3eb16e7ebb34b86045118715304402923
Piętka, Łukasz <lukasz.pietka@tvp.pl>
2018-10-03 11:49:00 +0200

    nowa wersja pliku

---
+++ weather_scrap.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+urlcam='https://www.traxelektronik.pl/pogoda/kamery/kamera.php?pkamnum='
+urlmeteo='https://www.traxelektronik.pl/pogoda/stacja/stacja.php?stid='
+
+kopytow_id = 141
+pulawska_id = 138
+
+r=reques
